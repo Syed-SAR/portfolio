8f1ec74e886ffc1c86c85121b362390c7968010e
Syed Abdul Rahman <abdulrahman9205@gmail.com>
2025-02-15 00:55:15 +0800

    Completed About section

--- index.js
+++ index.js
@@ -9,8 +9,18 @@ var typed = new Typed('.element', {
 var wh = window.innerHeight-50; // as soon as element touches bottom with offset event starts
 var element = document.querySelector(".sec1-dmush1"); //element
 
-window.addEventListener('scroll', function(){ 
-    if(scrollpos > (element.offsetTop - wh)){
-        element.classList.add("onScroll");
-    }
-});
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      const square = entry.target.querySelector('.square');
+  
+      if (entry.isIntersecting) {
+        square.classList.add('square-animation');
+        return; // if we added the class, exit the function
+      }
+  
+      // We're not intersecting, so remove the class!
+      square.classList.remove('square-animation');
+    });
+  });
+  
+  observer.observe(document.querySelector('.about__right-a'));
